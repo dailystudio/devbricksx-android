ace3c608994a565cb020062ecb8d13d71a863fb1
Nan YE <dailystudio2010@gmail.com>
2020-05-09 00:46:28 +0800

    [android][DEV]: sample codes project restructure;

--- android/sample-notebook/src/androidTest/java/com/dailystudio/devbricksx/notebook/ExampleInstrumentedTest.kt
+++ android/sample-notebook/src/androidTest/java/com/dailystudio/devbricksx/notebook/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.dailystudio.devbricksx.devbricksx.devbricksxsample
+package com.dailystudio.devbricksx.notebook
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.dailystudio.devbrickx.devbricksx.devbricksxsample", appContext.packageName)
+        assertEquals("com.dailystudio.devbricksx.notebook", appContext.packageName)
     }
 }
