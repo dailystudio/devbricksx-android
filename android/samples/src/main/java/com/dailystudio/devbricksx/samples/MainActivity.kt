11950aec29b342ba910f7bcf67254dbea5147c4c
Nan YE <dailystudio2010@gmail.com>
2020-05-12 17:53:50 +0800

    [android][DOC]: update documents for devbricksx-kotlin-annotations; [android][DOC]: update implementation of samples;

--- android/samples/src/main/java/com/dailystudio/devbricksx/samples/MainActivity.kt
+++ android/samples/src/main/java/com/dailystudio/devbricksx/samples/MainActivity.kt
@@ -8,8 +8,24 @@ import com.dailystudio.devbricksx.samples.quickstart.UserDatabase
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        val LAST_NAMES = arrayOf(
+                "Kelley", "Carter", "Salazar", "Osborne", "Santiago",
+                "Luna", "Wilson", "Craig", "Willis", "Abeln"
+        )
+
+        val FIRST_NAMES = arrayOf(
+                "Tanya", "Miriam", "Julie", "Leona", "Mabel",
+                "Emelia", "Sofia", "Haleema", "Francis", "Elin"
+        )
+
+        val RANDOM = Random(System.currentTimeMillis())
+        const val NAMES_COUNT = 20
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,14 +41,16 @@ class MainActivity : AppCompatActivity() {
 
             database.clearAllTables()
 
-            for (i in 0..10) {
-                for (j in 0..10) {
-                    val user = User(0,
-                            "firstName$j", "lastName$i")
+            for (i in 0..NAMES_COUNT) {
+                val fIndex = RANDOM.nextInt(FIRST_NAMES.size)
+                val lIndex = RANDOM.nextInt(LAST_NAMES.size)
 
-                    database.userDao().insert(user)
-                    delay(500)
-                }
+                val user = User(0,
+                        FIRST_NAMES[fIndex],
+                        LAST_NAMES[lIndex])
+
+                database.userDao().insert(user)
+                delay(200)
             }
         }
     }
