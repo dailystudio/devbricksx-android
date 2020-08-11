d49c698c0e14b904fd78e31035c9bc0a4cfb1070
Nan YE <dailystudio2010@gmail.com>
2020-08-11 16:26:53 +0800

    [devbricksx-kotlin-annotation][DEV]: keep annotation of SharedPreference in runtime; [devbricksx][REL]: update to version 1.1.0;

--- devbricksx-kotlin-annotations/src/main/java/com/dailystudio/devbricksx/annotations/SharedPreference.kt
+++ devbricksx-kotlin-annotations/src/main/java/com/dailystudio/devbricksx/annotations/SharedPreference.kt
@@ -1,10 +1,10 @@
 package com.dailystudio.devbricksx.annotations
 
-@Retention(AnnotationRetention.SOURCE)
+@Retention(AnnotationRetention.BINARY)
 @Target(AnnotationTarget.CLASS)
 annotation class SharedPreference()
 
-@Retention(AnnotationRetention.SOURCE)
+@Retention(AnnotationRetention.BINARY)
 @Target(AnnotationTarget.FIELD)
 annotation class PreferenceValue(val prefName: String = "",
                                  val defaultValueStr: String = "")
