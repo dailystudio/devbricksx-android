67cb98ad3418287f4b6309154a139f76e237cfd9
Nan YE <dailystudio2010@gmail.com>
2021-04-12 23:03:33 +0800

    [*][DEV]: publish artifacts to Maven Central instead of Jcenter;

---
+++ publishReleasesToMavenCentral.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+artifactsToPublish=(
+  "devbricksx"
+  "devbricksx-java-annotations"
+  "devbricksx-java-compiler"
+  "devbricksx-kotlin-annotations"
+  "devbricksx-kotlin-compiler"
+  "devbricksx-audio"
+  "devbricksx-camera"
+)
+
+echo "--------- [STEP 1: Building artifacts] ---------"
+for artifact in ${artifactsToPublish[@]}; do
+  task=":${artifact}:assemble"
+  echo "Processing artifact: [${artifact}] ..."
+  ./gradlew ${task} > /dev/null
+done
+
+echo "--------- [STEP 2: Publishing artifacts] ---------"
+for artifact in ${artifactsToPublish[@]}; do
+  task=":${artifact}:publish"
+  echo "Uploading artifact: [${artifact}] ..."
+  ./gradlew ${task} > /dev/null
+done
+
+#
+#./gradlew clean
+#
+#./updateVersionInDocs.sh
+#./updateRoomVersionInDocs.sh
