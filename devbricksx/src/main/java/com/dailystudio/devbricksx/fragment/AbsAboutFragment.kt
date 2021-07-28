1d1687be68a926d510f80b6e345d7d97fde99e3f
Nan YE <dailystudio2010@gmail.com>
2021-07-28 11:10:44 +0800

    [devbricksx][DEV]: adjust layout and implementation of AbsAboutFragment to increase the flexibility of extensions;

--- devbricksx/src/main/java/com/dailystudio/devbricksx/fragment/AbsAboutFragment.kt
+++ devbricksx/src/main/java/com/dailystudio/devbricksx/fragment/AbsAboutFragment.kt
@@ -11,8 +11,12 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatDialogFragment
+import androidx.lifecycle.lifecycleScope
 import com.dailystudio.devbricksx.R
 import com.dailystudio.devbricksx.development.Logger
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 abstract class AbsAboutFragment : AppCompatDialogFragment() {
 
@@ -20,21 +24,54 @@ abstract class AbsAboutFragment : AppCompatDialogFragment() {
         val context = requireContext()
 
         val dialogView: View = LayoutInflater.from(context).inflate(
-                R.layout.fragment_about, null)
+            fragmentLayoutResource, null)
 
-        val thumbView: ImageView? = dialogView.findViewById(R.id.about_app_thumb)
-        thumbView?.let {
-            val thumbResId = appThumbResource
-            if (thumbResId <= 0) {
-                it.visibility = View.GONE
-            } else {
-                it.visibility = View.VISIBLE
-                it.setImageResource(thumbResId)
-            }
+        val thumbView: View? = dialogView.findViewById(R.id.about_app_thumb)
+        bindThumb(thumbView)
+
+        val versionView: View? = dialogView.findViewById(R.id.about_app_ver)
+        bindVersion(versionView)
+
+        val nameView: View? = dialogView.findViewById(R.id.about_app_name)
+        bindName(nameView)
+
+        val descView: View? = dialogView.findViewById(R.id.about_app_desc)
+        bindDesc(descView)
+
+        val appIconView: View? = dialogView.findViewById(R.id.about_app_icon)
+        bindIcon(appIconView)
+
+        val builder = AlertDialog.Builder(context)
+                .setView(dialogView)
+                .setPositiveButton(android.R.string.ok
+                ) { _, _ -> }
+
+        return builder.create()
+    }
+
+    protected open fun bindThumb(view: View?) {
+        val thumbView = view as? ImageView ?: return
+
+        val thumbResId = appThumbResource
+        if (thumbResId <= 0) {
+            thumbView.visibility = View.GONE
+        } else {
+            thumbView.visibility = View.VISIBLE
+            thumbView.setImageResource(thumbResId)
         }
+    }
 
-        val versionView: TextView? = dialogView.findViewById(R.id.about_app_ver)
-        versionView?.let {
+    protected open fun bindIcon(view: View?) {
+        val appIconView = view as? ImageView ?: return
+
+        appIconView.setImageResource(appIconResource)
+    }
+
+    protected open fun bindVersion(view: View?) {
+        val versionView = view as? TextView ?: return
+        val context = requireContext()
+
+        lifecycleScope.launch(Dispatchers.IO) {
             var verName = getString(android.R.string.unknownName)
 
             val packageManager = context.packageManager
@@ -50,41 +87,39 @@ abstract class AbsAboutFragment : AppCompatDialogFragment() {
                 verName = info.versionName
             }
 
-            it.text = verName
-        }
-
-        val nameView: TextView? = dialogView.findViewById(R.id.about_app_name)
-        nameView?.text = appName
-
-        val descView: TextView? = dialogView.findViewById(R.id.about_app_desc)
-        descView?.let {
-            if (hasHtmlDescription()) {
-                it.autoLinkMask = 0
-                it.movementMethod = LinkMovementMethod.getInstance()
-            } else {
-                it.autoLinkMask = Linkify.EMAIL_ADDRESSES or Linkify.WEB_URLS
+            withContext(Dispatchers.Main) {
+                versionView.text = verName
             }
+        }
+    }
 
-            it.text = appDescription
+    protected open fun bindName(view: View?) {
+        val nameView = view as? TextView ?: return
+        nameView.text = appName
+    }
+
+    protected open fun bindDesc(view: View?) {
+        val descView = view as? TextView ?: return
+
+        if (hasHtmlDescription()) {
+            descView.autoLinkMask = 0
+            descView.movementMethod = LinkMovementMethod.getInstance()
+        } else {
+            descView.autoLinkMask = Linkify.EMAIL_ADDRESSES or Linkify.WEB_URLS
         }
 
-        val appIconView: ImageView? = dialogView.findViewById(R.id.about_app_icon)
-        appIconView?.setImageResource(appIconResource)
-
-        val builder = AlertDialog.Builder(context)
-                .setView(dialogView)
-                .setPositiveButton(android.R.string.ok
-                ) { _, _ -> }
-
-        return builder.create()
+        descView.text = appDescription
     }
 
     protected fun hasHtmlDescription(): Boolean {
         return false
     }
 
+    protected open val fragmentLayoutResource: Int
+        get() = R.layout.fragment_about
+
     protected open val appThumbResource: Int
-        protected get() = -1
+        get() = -1
 
     abstract val appName: CharSequence?
     abstract val appDescription: CharSequence?
