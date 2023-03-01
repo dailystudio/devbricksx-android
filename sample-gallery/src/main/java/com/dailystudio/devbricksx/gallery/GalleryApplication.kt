136a46b84291ded7e2de160f0bca46f6cc7b2d01
Nan YE <dailystudio2010@gmail.com>
2023-03-01 21:14:12 +0800

    [samples-gallery][DEV]: update Coil dependencies to 2.x;

--- sample-gallery/src/main/java/com/dailystudio/devbricksx/gallery/GalleryApplication.kt
+++ sample-gallery/src/main/java/com/dailystudio/devbricksx/gallery/GalleryApplication.kt
@@ -1,11 +1,45 @@
 package com.dailystudio.devbricksx.gallery
 
+import android.content.Context
+import android.util.Log
+import coil.Coil
+import coil.ImageLoader
 import com.dailystudio.devbricksx.app.DevBricksMultiDexApplication
+import com.dailystudio.devbricksx.development.Logger
+
+class CustomizedLogger: coil.util.Logger {
+
+    override var level: Int = Log.DEBUG
+        get() = Log.DEBUG
+
+    override fun log(tag: String, priority: Int, message: String?, throwable: Throwable?) {
+        Logger.debug("[$tag, p:$priority]: $message")
+    }
+
+}
 
 class GalleryApplication : DevBricksMultiDexApplication() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        setupImageLoader(this)
+    }
 
     override fun isDebugBuild(): Boolean {
         return BuildConfig.DEBUG
     }
 
+    private fun setupImageLoader(context: Context) {
+        val imageLoader = ImageLoader.Builder(context)
+            .apply {
+                if (BuildConfig.DEBUG) {
+                    allowHardware(false)
+                    logger(CustomizedLogger())
+                }
+            }
+            .build()
+
+        Coil.setImageLoader(imageLoader)
+    }
 }
