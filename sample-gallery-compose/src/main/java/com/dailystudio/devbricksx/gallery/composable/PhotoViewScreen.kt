8e534091aca5568d0e92705d82afc0f1117e8280
Nan YE <dailystudio2010@gmail.com>
2023-08-08 16:06:08 +0800

    [sample-gallery-compose][DEV]: implement image loading with thumbnail first;

--- sample-gallery-compose/src/main/java/com/dailystudio/devbricksx/gallery/composable/PhotoViewScreen.kt
+++ sample-gallery-compose/src/main/java/com/dailystudio/devbricksx/gallery/composable/PhotoViewScreen.kt
@@ -2,20 +2,50 @@ package com.dailystudio.devbricksx.gallery.composable
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import coil.compose.AsyncImagePainter
+import coil.compose.SubcomposeAsyncImage
+import coil.compose.SubcomposeAsyncImageContent
 import coil.compose.rememberAsyncImagePainter
 import com.dailystudio.devbricksx.gallery.db.PhotoItem
 
 @Composable
 fun PhotoViewScreen(item: PhotoItem?) {
     if (item != null) {
-        Image(
+        ImageWithThumb(
+            item.thumbnailUrl,
+            item.downloadUrl,
             modifier = Modifier.fillMaxSize(),
-            painter = rememberAsyncImagePainter(item.thumbnailUrl),
             contentScale = ContentScale.Crop,
             contentDescription = null,
         )
+    }
+}
+
+@Composable
+fun ImageWithThumb(
+    thumbnail: String,
+    highQuality: String,
+    modifier: Modifier,
+    contentScale: ContentScale = ContentScale.Crop,
+    contentDescription: String? = null,
+) {
+    SubcomposeAsyncImage(
+        model = highQuality,
+        modifier = modifier,
+        contentScale = contentScale,
+        contentDescription = contentDescription) {
+
+        val state = painter.state
+        if (state is AsyncImagePainter.State.Loading || state is AsyncImagePainter.State.Error) {
+            SubcomposeAsyncImageContent(
+                painter = rememberAsyncImagePainter(thumbnail),
+            )
+        } else {
+            SubcomposeAsyncImageContent()
+        }
     }
 }
