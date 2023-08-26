dbf52266286b574e18b349dc1ead6f515e935440
Nan YE <dailystudio2010@gmail.com>
2023-08-27 00:05:15 +0800

    [samples][DEV]: add options menu back;

--- samples/src/main/java/com/dailystudio/devbricksx/samples/usecase/fragment/UseCasesFragmentExt.kt
+++ samples/src/main/java/com/dailystudio/devbricksx/samples/usecase/fragment/UseCasesFragmentExt.kt
@@ -2,16 +2,28 @@ package com.dailystudio.devbricksx.samples.usecase.fragment
 
 import android.content.Intent
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.RecyclerView
 import com.dailystudio.devbricksx.app.activity.ActivityLauncher
 import com.dailystudio.devbricksx.samples.Constants
+import com.dailystudio.devbricksx.samples.R
 import com.dailystudio.devbricksx.samples.usecase.UseCase
 import com.dailystudio.devbricksx.samples.usecase.model.UseCaseViewModelExt
 import kotlinx.coroutines.flow.Flow
 
 class UseCasesFragmentExt : UseCasesListFragment() {
 
+    override fun setupViews(fragmentView: View) {
+        super.setupViews(fragmentView)
+        val activity = requireActivity()
+        if (activity is AppCompatActivity) {
+            activity.setSupportActionBar(fragmentView.findViewById(R.id.topAppBar))
+        }
+
+        setHasOptionsMenu(true)
+
+    }
     override fun onItemClick(recyclerView: RecyclerView, itemView: View, position: Int, item: UseCase, id: Long) {
         super.onItemClick(recyclerView, itemView, position, item, id)
 
