4fb98951f3342956dd135eae4c39381ecf7f8943
Nan YE <dailystudio2010@gmail.com>
2023-09-13 22:42:03 +0800

    [samples][DEV]: code refactor the webview case;

--- samples/src/main/java/com/dailystudio/devbricksx/samples/webview/fragment/WebViewFragment.kt
+++ samples/src/main/java/com/dailystudio/devbricksx/samples/webview/fragment/WebViewFragment.kt
@@ -1,7 +1,6 @@
 package com.dailystudio.devbricksx.samples.webview.fragment
 
-import android.app.Activity
-import android.content.Intent
+import android.annotation.SuppressLint
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
@@ -12,16 +11,34 @@ import android.webkit.ValueCallback
 import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import androidx.activity.result.contract.ActivityResultContracts
+import com.dailystudio.devbricksx.development.Logger
 import com.dailystudio.devbricksx.fragment.AbsPermissionsFragment
 import com.dailystudio.devbricksx.samples.R
-import org.jetbrains.annotations.Nullable
 
 
 class WebViewFragment: AbsPermissionsFragment() {
-    private var mUploadMessage: ValueCallback<Array<Uri>>? = null
-    private val REQUEST_SELECT_FILE = 100
+
+    private var pendingInput: String? = null
+    private var pendingFilePathCallback: ValueCallback<Array<Uri>>? = null
+
+    private val pickImagesLauncher =
+        registerForActivityResult(ActivityResultContracts.GetContent()) { uri ->
+            val callback = pendingFilePathCallback ?: return@registerForActivityResult
+
+            if (uri != null) {
+                callback.onReceiveValue(arrayOf(uri))
+            } else {
+                callback.onReceiveValue(null)
+            }
+
+            pendingFilePathCallback = null
+        }
 
     private var webView: WebView? = null
+
+    override val autoCheckPermissions: Boolean
+        get() = false
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -35,55 +52,49 @@ class WebViewFragment: AbsPermissionsFragment() {
         super.onViewCreated(view, savedInstanceState)
 
         webView = view.findViewById(R.id.webview)
-        webView?.settings?.setJavaScriptEnabled(true)
+        webView?.settings?.javaScriptEnabled = true
+
         webView?.webViewClient = WebViewClient()
         webView?.webChromeClient = object : WebChromeClient() {
+
             override fun onShowFileChooser(
                 webView: WebView,
                 filePathCallback: ValueCallback<Array<Uri>>,
                 fileChooserParams: FileChooserParams
             ): Boolean {
-                if (mUploadMessage != null) {
-                    mUploadMessage!!.onReceiveValue(null)
+                var input = buildString {
+                    val types = fileChooserParams.acceptTypes
+
+                    for ((i, t) in types.withIndex()) {
+                        append(t)
+                        if (i < types.size - 1) {
+                            append(", ")
+                        }
+                    }
                 }
-                mUploadMessage = filePathCallback
-                val intent = Intent(Intent.ACTION_GET_CONTENT)
-                intent.addCategory(Intent.CATEGORY_OPENABLE)
-                intent.type = "image/*"
-                startActivityForResult(
-                    Intent.createChooser(intent, "Select Picture"),
-                    REQUEST_SELECT_FILE
-                )
+
+                if (input.isBlank()) {
+                    input = "*/*"
+                }
+
+                Logger.debug("input: [$input]")
+
+                pendingInput = input
+                pendingFilePathCallback = filePathCallback
+
+                checkOrGrantPermissions()
+
                 return true
             }
+
         }
 
         loadHomepage()
     }
 
-
-    override fun onActivityResult(
-        requestCode: Int,
-        resultCode: Int,
-        data: Intent?
-    ) {
-        if (data == null) {
-            return
-        }
-
-        if (requestCode == REQUEST_SELECT_FILE) {
-            if (resultCode == Activity.RESULT_OK) {
-                if (mUploadMessage == null) return
-                val results = arrayOf(data.data!!)
-                mUploadMessage!!.onReceiveValue(results)
-                mUploadMessage = null
-            }
-        }
-        super.onActivityResult(requestCode, resultCode, data)
-    }
     private fun loadHomepage() {
-//        webView?.loadUrl("file:///android_asset/html/local_album.html")
-        webView?.loadUrl("https://wpa1.qq.com/YcO1i4YV?_type=wpa&qidian=true")
+        webView?.loadUrl("file:///android_asset/html/local_album.html")
+//        webView?.loadUrl("https://wpa1.qq.com/YcO1i4YV?_type=wpa&qidian=true")
     }
 
     override fun getPermissionsPromptViewId(): Int {
@@ -102,6 +113,9 @@ class WebViewFragment: AbsPermissionsFragment() {
     }
 
     override fun onPermissionsGranted(newlyGranted: Boolean) {
+        val input = pendingInput ?: return
+
+        pickImagesLauncher.launch(input)
     }
 
 }
