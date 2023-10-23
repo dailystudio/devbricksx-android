037d2b3da2cd2a3b82dbaab8e29075a8a100e60e
Nan YE <dailystudio2010@gmail.com>
2023-10-23 23:52:48 +0800

    [sample-notebook-*][DEV]: rename to Notes [sample-notebook][DEV]: generated default data of notebooks;

--- sample-notebook/src/main/java/com/dailystudio/devbricksx/notebook/MainActivity.kt
+++ sample-notebook/src/main/java/com/dailystudio/devbricksx/notebook/MainActivity.kt
@@ -15,51 +15,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        DummyDataUtils.createSampleNotes(this, this.lifecycleScope)
+
         setContentView(R.layout.activity_main)
-
-//        createSampleNotes()
-    }
-
-    private fun createSampleNotes() {
-        lifecycleScope.launch(Dispatchers.IO) {
-            val notebooks = arrayOf(
-                    "Games", "Tech",
-                    "Books", "Office",
-                    "Home"
-            )
-            val maxNotes = 5
-
-            val database = NotesDatabase.getDatabase(
-                    this@MainActivity)
-
-            val notes = mutableListOf<Note>()
-            for ((i, displayName) in notebooks.withIndex()) {
-                val notebookId = i + 1
-                val nb = Notebook(notebookId).apply {
-                    name = displayName
-                    created = Date()
-                    lastModified = created
-                }
-
-                notes.clear()
-                for (j in 0 until maxNotes) {
-                    val noteId = notebookId * maxNotes + j + 1
-
-                    notes.add(Note(noteId).apply {
-                        notebook_id = notebookId
-                        title = "$displayName $j"
-                        desc = "Write something for $displayName $j"
-
-                        created = Date()
-                        lastModified = created
-                    })
-                }
-
-                database.notebookDao().insertOrUpdate(nb)
-                database.noteDao().insertOrUpdate(notes)
-            }
-
-        }
     }
 
 }
