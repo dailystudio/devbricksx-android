4bf6d1cd713ad5e71577283bcfef9b170bca6412
Nan YE <dailystudio2010@gmail.com>
2024-03-19 22:37:29 +0800

    [*][REL]: update version to 1.8.9;

--- gradle-plugin/devkit/src/main/kotlin/com/dailystudio/devbricksx/Dependencies.kt
+++ gradle-plugin/devkit/src/main/kotlin/com/dailystudio/devbricksx/Dependencies.kt
@@ -2,7 +2,7 @@ package com.dailystudio.devbricksx
 
 object Dependencies {
 
-    const val DEV_BRICKS_X_VERSION = "1.8.8"
+    const val DEV_BRICKS_X_VERSION = "1.8.9"
     const val ROOM_VERSION = "2.6.1"
     const val KOTLIN_COMPILER_EXT_VERSION = "1.5.4"
 
